4da6dd32df9bfca23693b7dd88e0030ee6c66446
unknown <argiepairpagbunocan@gmail.com>
2025-04-23 08:54:18 +0800

    first commit

---
+++ MymidtermApi/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Footer({ className }) {
+  return (
+    <footer className={`text-light mt-5 ${className}`}>
+    
+    </footer>
+  );
+}
+
+export default Footer;
